cd0ad8f1e74618f8718ea88219706bff56ea6948
suden123 <suden123>
2018-11-12 22:11:44 +0530

    commited 4th

--- src/gitpractice/thirdProg.java
+++ src/gitpractice/thirdProg.java
@@ -3,8 +3,7 @@ package gitpractice;
 public class thirdProg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome third....");
 	}
 
 }
