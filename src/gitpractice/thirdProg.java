bd43f294de03e4c01285afb99c5e98b34bbe22ef
suden123 <suden123>
2018-11-12 22:26:58 +0530

    i gave one more for loop

--- src/gitpractice/thirdProg.java
+++ src/gitpractice/thirdProg.java
@@ -5,6 +5,7 @@ public class thirdProg {
 	public static void main(String[] args) {
 		System.out.println("Welcome third....");
 	
-for(int i = 0;i<10;i++){
+for(int i = 0;i<4;i++){
+	for(int j=i;j<4-i;j++){
 }
-}}
+}}}
